49ee980a808f67f8ac186071b0935c348c9d7551
Siddharth Chhatbar <siddharth3901@gmail.com>
2025-03-23 12:10:04 +0900

    sidebar init

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { AppSidebar } from "./components/AppSidebar";
+import { SidebarProvider } from "./components/ui/sidebar";
+
 const App = () => {
-  return <div className="text-4xl text-red-400">App</div>;
+  return <div className="text-4xl text-red-400">
+    <SidebarProvider>
+      <AppSidebar />
+    </SidebarProvider>
+  </div>;
 };
 
 export default App;
